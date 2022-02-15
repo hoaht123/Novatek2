eab1c16dd1582068c747eef8c18b700dab053155
Hoa <vongochoa30@gmail.com>
2022-02-15 10:47:11 +0700

    update

---
+++ database/migrations/2022_02_15_033558_create_roles.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Roles', function (Blueprint $table) {
+            $table->id('roles_id');
+            $table->string('role_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Roles');
+    }
+}
